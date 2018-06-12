f41444a50c7f414a7933e3e83961f62860d0b6a2
Ryan Manfred <rmanfred@purdue.edu>
2018-06-12 10:48:57 -0400

    add messages to chats state

--- src/Chat.js
+++ src/Chat.js
@@ -3,11 +3,20 @@ import ChatHeader from './ChatHeader'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 class Chat extends Component {
+  constructor(){
+    super()
+    this.state ={
+      messages: [
+        { id: 1, userName: 'dstrus', body: 'Chatting up a storm, yo!' },
+         { id: 2, userName: 'dpalazzo', body: 'This guy is so annoying. I hate my job.' },
+      ]
+    }
+  }
     render() {
       return (
         <div className="Chat">
           <ChatHeader/>
-          <MessageList/>
+          <MessageList messages={this.state.messages}/>
           <MessageForm/>
         </div>
       );
